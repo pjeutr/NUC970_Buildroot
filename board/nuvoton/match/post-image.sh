2b601a98db6ad92fa413457b486b9b70f9abc39b
Peter <github@pjeutr.com>
2021-12-30 23:08:44 +0100

    v0.7.1

--- board/nuvoton/match/post-image.sh
+++ board/nuvoton/match/post-image.sh
@@ -12,11 +12,15 @@
 
 #LEB size: 126976 bytes (124 KiB), min./max. I/O unit sizes: 2048 bytes/2048 bytes
 #FS size: 93073408 bytes (88 MiB, 733 LEBs), journal size 9023488 bytes (8 MiB, 72 LEBs)
+
+#rootfs
+#LEB size: 126976 bytes (124 KiB), min./max. I/O unit sizes: 2048 bytes/2048 bytes
+#FS size: 33554432 bytes (88 MiB, 733 LEBs), journal size 9023488 bytes (8 MiB, 72 LEBs)
 mkfs.ubifs -F -d maasland_app -e 0x1F000 -c 768 -m 0x800 -o output/images/maasland_app.ubifs
 ubinize -o output/images/maasland_app.ubi -m 0x800 -p 0x20000 -s 2048 board/nuvoton/match/ubinize.cfg
 
 # -e --leb-size=SIZE
-# -c --max-leb-cnt=COUNT
+# -c --max-leb-cnt=COUNT <--
 # -m --min-io-size=SIZE
 
 # -m --min-io-size=<bytes>
