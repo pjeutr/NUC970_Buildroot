8e2f7ec6b10f141df744bb52f58c3f1d1e37ed0e
pjeutr <pjeutr@github.com>
2023-11-01 13:48:06 +0000

    add rtc and php-meminfo

--- package/php-meminfo/php-meminfo.mk
+++ package/php-meminfo/php-meminfo.mk
@@ -6,16 +6,17 @@
 
 PHP_MEMINFO_VERSION = 1.1.1
 PHP_MEMINFO_SITE = $(call github,BitOne,php-meminfo,v$(PHP_MEMINFO_VERSION))
-PHP_MEMINFO_CONF_OPTS = --enable-meminfo
+PHP_MEMINFO_SUBDIR = extension/php7 
+PHP_MEMINFO_CONF_OPTS = --with-php-config=$(STAGING_DIR)/usr/bin/php-config \
+        --enable-meminfo
 # phpize does the autoconf magic
 PHP_MEMINFO_DEPENDENCIES = php host-autoconf
 PHP_MEMINFO_LICENSE = PHP
 PHP_MEMINFO_LICENSE_FILES = LICENSE
 
-https://github.com/BitOne/php-meminfo/archive/refs/tags/v1.1.1.tar.gz
 
 define PHP_MEMINFO_PHPIZE
-	(cd $(@D); \
+	(cd $(@D)/extension/php7; \
 		PHP_AUTOCONF=$(HOST_DIR)/usr/bin/autoconf \
 		PHP_AUTOHEADER=$(HOST_DIR)/usr/bin/autoheader \
 		$(STAGING_DIR)/usr/bin/phpize)
