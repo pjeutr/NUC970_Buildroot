61ef9d23be0cdd2c9debfe8de87f5458b4106759
pjeutr <pjeutr@github.com>
2024-04-16 09:46:00 +0000

    v188

--- sdcard/makeSD2.sh
+++ sdcard/makeSD2.sh
@@ -9,3 +9,4 @@ curl --form 'file=@sdcard/matchSDvx_x_x.img' http://mslnd.nl:8080
 
 #echo "curl -L https://free.keep.sh/xxx/php_errors.log > php_errors.log"
 echo "curl -O http://mslnd.nl:8080/file/lLG55FITRCbhQ46d/uE1sia3aohJpjoyH/matchSDvx_x_x.img"
+echo "mv matchSDvx_x_x.img ~/maaslandserver/public/firmware/matchSDv1_8_8.img"
